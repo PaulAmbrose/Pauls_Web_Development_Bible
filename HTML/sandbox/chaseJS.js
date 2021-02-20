9d548979a866e64d349d29f8650fe607d3df25f4
Paul Ambrose <pablo_rose34@hotmail.com>
2021-02-20 14:45:54 +0000

    update
    Signed-off-by: Paul Ambrose <pablo_rose34@hotmail.com>

--- HTML/sandbox/chaseJS.js
+++ HTML/sandbox/chaseJS.js
@@ -7,19 +7,63 @@ let section5 = document.getElementById("section5");
 let section6 = document.getElementById("section6");
 let section7 = document.getElementById("section7");
 
-section1.addEventListener("click", claimChaser);
-section2.addEventListener("click", claimChaser);
-section3.addEventListener("click", claimChaser);
-section4.addEventListener("click", claimChaser);
-section5.addEventListener("click", claimChaser);
-section6.addEventListener("click", claimChaser);
-section7.addEventListener("click", claimChaser);
+section1.addEventListener("click", claimChaser1);
+section2.addEventListener("click", claimChaser2);
+section3.addEventListener("click", claimChaser3);
+section4.addEventListener("click", claimChaser4);
+section5.addEventListener("click", claimChaser5);
+section6.addEventListener("click", claimChaser6);
+section7.addEventListener("click", claimChaser7);
 
 
-function claimChaser(){
-    section.classList.add("selectedChaser")
-    }
+function claimChaser1(){
+    section1.classList.add("selectedPlayer")
+    };
+    function claimChaser2(){
+        section2.classList.add("selectedPlayer")
+        };
+        function claimChaser3(){
+            section3.classList.add("selectedPlayer")
+            };
+            function claimChaser4(){
+                section4.classList.add("selectedPlayer")
+                };
+                function claimChaser5(){
+                    section5.classList.add("selectedPlayer")
+                    };
+                    function claimChaser6(){
+                        section6.classList.add("selectedPlayer")
+                        };
+                        function claimChaser7(){
+                            section7.classList.add("selectedPlayer")
+                            };
 
-function claimPlayer(){
+                            section1.addEventListener("contextmenu", claimPlayer1);
+                            section2.addEventListener("contextmenu", claimPlayer2);
+                            section3.addEventListener("contextmenu", claimPlayer3);
+                            section4.addEventListener("contextmenu", claimPlayer4);
+                            section5.addEventListener("contextmenu", claimPlayer5);
+                            section6.addEventListener("contextmenu", claimPlayer6);
+                            section7.addEventListener("contextmenu", claimPlayer7);
 
-    }
+                            function claimPlayer1(){
+                                section1.classList.add("selectedPlayer")
+                                };
+                                function claimPlayer2(){
+                                    section2.classList.add("selectedPlayer")
+                                    };
+                                    function claimPlayer3(){
+                                        section3.classList.add("selectedPlayer")
+                                        };
+                                        function claimPlayer4(){
+                                            section4.classList.add("selectedPlayer")
+                                            };
+                                            function claimPlayer5(){
+                                                section5.classList.add("selectedPlayer")
+                                                };
+                                                function claimPlayer6(){
+                                                    section6.classList.add("selectedPlayer")
+                                                    };
+                                                    function claimPlayer7(){
+                                                        section7.classList.add("selectedPlayer")
+                                                        };
