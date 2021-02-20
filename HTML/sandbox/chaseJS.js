b417d2774339189b538f7da138f1d896d6d71e0f
Paul Ambrose <pablo_rose34@hotmail.com>
2021-02-20 14:24:21 +0000

    update
    Signed-off-by: Paul Ambrose <pablo_rose34@hotmail.com>

--- HTML/sandbox/chaseJS.js
+++ HTML/sandbox/chaseJS.js
@@ -1,3 +1,25 @@
-document.getElementById("section1").addEventListener("click", changeColor)
 
-function: changeColor{document.body.style.backgroundColor = "red";}
+let section1 = document.getElementById("section1");
+let section2 = document.getElementById("section2");
+let section3 = document.getElementById("section3");
+let section4 = document.getElementById("section4");
+let section5 = document.getElementById("section5");
+let section6 = document.getElementById("section6");
+let section7 = document.getElementById("section7");
+
+section1.addEventListener("click", claimChaser);
+section2.addEventListener("click", claimChaser);
+section3.addEventListener("click", claimChaser);
+section4.addEventListener("click", claimChaser);
+section5.addEventListener("click", claimChaser);
+section6.addEventListener("click", claimChaser);
+section7.addEventListener("click", claimChaser);
+
+
+function claimChaser(){
+    section.classList.add("selectedChaser")
+    }
+
+function claimPlayer(){
+
+    }
