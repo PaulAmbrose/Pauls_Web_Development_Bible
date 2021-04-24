f538aa3277f46fc7a2017f58d832fce0b4bcdbf3
Paul Ambrose <pablo_rose34@hotmail.com>
2021-04-24 14:24:00 +0100

    update
    Signed-off-by: Paul Ambrose <pablo_rose34@hotmail.com>

--- JS/Projects/5-sidebar/setup/app.js
+++ JS/Projects/5-sidebar/setup/app.js
@@ -1,3 +1,8 @@
 const toggleBtn = document.querySelector('.sidebar-toggle');
 const closeBtn = document.querySelector('.close-btn');
-const sidebar = document.querySelector('.sidebar');
+const sideBar = document.querySelector('.sidebar');
+
+toggleBtn.addEventListener('click', function(){
+    console.log("click recoreded");
+    sideBar.classList.toggle('.show-sidebar');
+});
