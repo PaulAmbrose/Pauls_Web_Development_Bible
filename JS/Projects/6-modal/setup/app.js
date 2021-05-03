e8d36a8667b26516f900da32b80711bbae2477c1
Paul Ambrose <pablo_rose34@hotmail.com>
2021-05-03 13:31:08 +0100

    update
    Signed-off-by: Paul Ambrose <pablo_rose34@hotmail.com>

--- JS/Projects/6-modal/setup/app.js
+++ JS/Projects/6-modal/setup/app.js
@@ -1,7 +1,7 @@
 
-const modalButton = document.querySelector('modal-btn');
-const modalOverlay = document.querySelector('modal-overlay');
-const modalClose = document.querySelector('close-btn');
+const modalButton = document.querySelector('.modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const modalClose = document.querySelector('.close-btn');
 
 modalButton.addEventListener('click', function () {
   modalOverlay.classList.toggle('open-modal');
