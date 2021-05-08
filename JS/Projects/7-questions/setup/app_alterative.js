afce8eabbc15b33c86d92bee1e9122c1c161cb52
Paul Ambrose <pablo_rose34@hotmail.com>
2021-05-08 12:45:58 +0100

    update
    Signed-off-by: Paul Ambrose <pablo_rose34@hotmail.com>

--- JS/Projects/7-questions/setup/app_alterative.js
+++ JS/Projects/7-questions/setup/app_alterative.js
@@ -2,4 +2,7 @@ const questions = document.querySelectorAll('.question');
 
 questions.forEach(function (question) {
   const btn = question.querySelector('.question-btn');
-})
+  btn.addEventListener('click', function () {
+    question.classList.toggle('show-text');
+  })
+});
